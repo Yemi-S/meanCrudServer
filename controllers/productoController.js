8e93a28a0b960b1cd486c7b4be340a20b8597cdf
Yemi-S <zpfmgy@hotmail.com>
2022-05-23 14:57:02 -0500

    API - PUT method

--- controllers/productoController.js
+++ controllers/productoController.js
@@ -1,6 +1,6 @@
 const Producto = require("../models/Producto");
 
-
+// Crear Producto
 exports.crearProducto = async (req, res) =>{
     try{
         let producto;
@@ -17,10 +17,36 @@ exports.crearProducto = async (req, res) =>{
     
 }
 
+//Listar productos
 exports.obtenerProductos = async (req, res) =>{
     try{
         const productos = await Producto.find();
         res.json(productos);
+    }catch(error){
+        console.log(req.body);
+        res.status(500).send('Hubo un error');
+    }
+    
+}
+
+// Actualizar productos
+exports.actualizarProducto = async (req, res) =>{
+    try{
+        const { nombre, categoria, ubicacion, precio } = req.body;
+        let producto = await Producto.findById(req.params.id);
+
+        if (!producto) {
+            res.status(404).json({msg: 'No existe el producto'});
+        }
+
+        producto.nombre = nombre;
+        producto.categoria = categoria;
+        producto.ubicacion = ubicacion;
+        producto.precio = precio;
+
+        producto = await Producto.findOneAndUpdate({ _id: req.params.id}, producto, { new: true});
+        res.json(producto);
+        
     }catch(error){
         console.log(req.body);
         res.status(500).send('Hubo un error');
--- routes/producto.js
+++ routes/producto.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const productoController = require('../controllers/productoController')
 // api/productos
 router.post('/', productoController.crearProducto);
-router.get('/', productoController.obtenerProductos)
+router.get('/', productoController.obtenerProductos);
+router.put('/:id', productoController.actualizarProducto);
 module.exports = router;
